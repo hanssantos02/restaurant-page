d707ca22fd288bf29abd684dedea9f9744940406
Nexus <hansprielasantos02@gmail.com>
2026-09-15 12:33:47 +0800

    Finish Menu Button Logic in JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import { home } from "./home.js";
+import { menu } from "./menu.js";
+
+function showTab(loader) {
+    document.querySelector('#content').replaceChildren();
+    loader();
+}
+
+document.querySelector("#home").addEventListener("click", () => showTab(home));
+document.querySelector("#menu").addEventListener("click", () => showTab(menu));
+
 home();
